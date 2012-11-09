d68d602b9f9a836216de9b75f60198af1a086fc7
Christophe Hamerling <christophe.hamerling@gmail.com>
2012-11-09 11:09:19 +0100

    Add bin and configuration

---
+++ config.js
@@ -0,0 +1,22 @@
+{
+  "server": {
+    "host": "localhost",
+    "port": 3000,
+    "secure": false,
+    "workers": 2
+  },
+  
+  "database": {
+    "backend": "redis",
+    "settings": {
+      "host": "localhost",
+      "port": 6379,
+      "password": null
+    }
+  },
+  
+  "push": {
+    "error" : 10,
+    "success" : -1
+  }
+}
