ae1b51803fead9d17068643f33fd4ba563dcdae0
Christophe Hamerling <christophe.hamerling@gmail.com>
2012-11-12 11:27:38 +0100

    fix URL

--- benchmarks/run.sh
+++ benchmarks/run.sh
@@ -2,4 +2,4 @@
 
 ulimit -n 10000
 
-ab -n 100 -c 10 -p ./requests/wsn_notify.json -T "application/json" http://127.0.0.1:3000/api/v1/monitoring/dsb/wsn/
+ab -n 1000 -c 10 -p ./requests/wsn_newInNotifyOutput.json -T "application/json" http://127.0.0.1:3000/api/v1/monitoring/wsn/
